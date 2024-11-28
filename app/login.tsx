55d232df72414988d517ee64c851b4033f193014
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2024-11-28 13:44:55 +0700

    kirim login

--- app/login.tsx
+++ app/login.tsx
@@ -1,7 +1,54 @@
+import { useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
+
 export default function LoginPage () {
+  const [username, setUsername] = useState('doejohn');
+  const [password, setPassword] = useState('test@123');
+
+  const signIn = async () => {
+    const url = 'https://api.freeapi.app/api/v1/users/login';
+    const options = {
+      method: 'POST',
+      headers: {accept: 'application/json', 'content-type': 'application/json'},
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      })
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <View>
+      <Text>Login</Text>
+      <View>
+        <View>
+          <TextInput
+            placeholder="Masukkan username"
+            onChangeText={newUsername => setUsername(newUsername)}
+            value={username}
+          /> 
+        </View>
+
+        <View>
+          <TextInput
+            placeholder="Masukkan password"
+            onChangeText={password => setPassword(password)}
+            value={password}
+          /> 
+        </View>
+
+        <View>
+          <Button title="Login" onPress={signIn}/>
+        </View>
+      </View>
+    </View>
   );
 }
